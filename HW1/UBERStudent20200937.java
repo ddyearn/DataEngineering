29b295d713af060be55854364496aebbd7b4b73f
Yoo SeungYeon <115228974+ddyearn@users.noreply.github.com>
2023-05-13 13:45:18 +0900

    Update UBERStudent20200937.java

--- HW1/UBERStudent20200937.java
+++ HW1/UBERStudent20200937.java
@@ -1,6 +1,9 @@
+import java.io.IOException;
 import java.util.*;
-import java.io.*;
-import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Locale;
+import java.time.format.TextStyle;
 
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.fs.FileSystem;
@@ -9,118 +12,95 @@ import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-public class UBERStudent20200937 {
-	public static String parseToString(int dayNum) {
-		switch(dayNum){
-        	case 1:
-        		return "SUN";
-        	case 2:
-        		return "MON";
-        	case 3:
-        		return "TUE";
-        	case 4:
-        		return "WED";
-        	case 5:
-        		return "THR";
-        	case 6:
-        		return "FRI";
-        	case 7:
-        		return "SAT";
-        	default:
-        		return "NULL";
-		}
-	}
-	
-	public static class Map extends Mapper <LongWritable, Text, Text, Text> {
-		Text word = new Text();
-		Text val = new Text();
-		
-		public void map(LongWritable key, Text value, Context context) 
-				throws IOException, InterruptedException {
-			StringTokenizer itr = new StringTokenizer(value.toString(), ",");
-			if(itr.countTokens() < 4) return;
-			
-			String base_number =  itr.nextToken().trim();
-			String date = itr.nextToken().trim(); 
-			int active_vehicles = Integer.parseInt( itr.nextToken().trim() );
-			int trips = Integer.parseInt( itr.nextToken().trim() );
-			
-			String dayOfWeek = "";
-			try {
-				SimpleDateFormat transFormat = new SimpleDateFormat("dd/MM/yyyy");
-				Date dateObj = transFormat.parse(date);
-			
-				Calendar cal = Calendar.getInstance() ;
-				cal.setTime(dateObj);
-			     
-				int dayNum = cal.get(Calendar.DAY_OF_WEEK) ;
-				dayOfWeek = parseToString(dayNum);
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
-			
-			word.set(base_number + "," + dayOfWeek);
-			val.set(trips + "," + active_vehicles);
-			
-			context.write(word, val);
-		}
-		
-	}
-	
-	public static class Reduce extends Reducer <Text, Text, Text, Text> {
-		Text result = new Text();
-		
-		public void reduce(Text key, Iterable<Text> values, Context context) 
-				throws IOException, InterruptedException {
-			int total_active_vehicles = 0;
-			int total_trips = 0;
-			
-			for(Text val : values) {
-				StringTokenizer itr = new StringTokenizer(val.toString(), ",");
-				int trips = Integer.parseInt( itr.nextToken().trim() );
-				int active_vehicles = Integer.parseInt( itr.nextToken().trim() );
-				
-				total_active_vehicles += active_vehicles;
-				total_trips += trips;
-			}
-			
-			result.set(total_trips + "," + total_active_vehicles);
-			
-			context.write(key, result);
-		}
-	}
-	
-    public static void main(String[] args) throws Exception {
-    	Configuration conf = new Configuration();
-    	String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-		if (otherArgs.length != 2)
-		{
-			System.err.println("Usage: UBER <in> <out>");
-			System.exit(2);
-		}
-    	
-    	Job job = new Job(conf, "UBERStudent20200937");
-    	job.setJarByClass(UBERStudent20200937.class);
-    	
-    	job.setMapperClass(Map.class);
-    	job.setReducerClass(Reduce.class);
-    	job.setCombinerClass(Reduce.class);
-    	
-    	job.setOutputKeyClass(Text.class);
-    	job.setOutputValueClass(Text.class);
-    	
-    	job.setInputFormatClass(TextInputFormat.class);
-    	job.setOutputFormatClass(TextOutputFormat.class);
-    	
-    	FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
-		
-    	FileSystem.get(job.getConfiguration()).delete(new Path(otherArgs[1]), true);
+public class UBERStudent20200937
+{
 
-    	job.waitForCompletion(true);
-    }
+        public static class UberMapper extends Mapper<LongWritable, Text, Text, Text>
+        {
+                private Text region_day = new Text();
+                private Text trips_vehicles = new Text();
+
+                public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
+                {
+                    //Write map code
+                    String line = value.toString();
+                    String[] uberInfo = line.split(",");
+                    String region = uberInfo[0];
+                    String date = uberInfo[1];
+                    String vehicles = uberInfo[2];
+                    String trips = uberInfo[3];
+
+                    // 1/15/2015 
+                    String[] dateInfo = date.split("/");
+                    String month = dateInfo[0];
+                    String day = dateInfo[1];
+                    String year = dateInfo[2];
+                    LocalDate date2 = LocalDate.of( Integer.parseInt(year),  Integer.parseInt(month),  Integer.parseInt(day));
+                    DayOfWeek dayOfWeek = date2.getDayOfWeek();
+                    String dayofweek = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.US).toUpperCase();
+                    if(dayofweek.equals("THU")) {
+                            dayofweek = "THR";
+                    }
+                    String region_day1 = region+","+dayofweek;
+                    String trips_vehicles1 = trips+","+vehicles;
+
+                    region_day.set(region_day1);
+                    trips_vehicles.set(trips_vehicles1);
+                    context.write(region_day, trips_vehicles);
+                }
+        }
+
+        public static class UberReducer extends Reducer<Text,Text,Text,Text>
+        {
+                private int tripSum = 0;
+                private int vehicleSum = 0;
+                private Text trips_vehicles = new Text();
+
+                public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException
+                {
+                    int tripSum = 0;
+                    int vehicleSum = 0;
+                    //Write reduce code
+                    for (Text val : values) {
+                        String line = val.toString();
+                        String[] trip_vehicle = line.split(",");
+                        
+                        String trip_string = trip_vehicle[0];
+                        String vehicle_string = trip_vehicle[1];
+
+                        int trip_int = Integer.parseInt(trip_string);
+                        int vehicle_int = Integer.parseInt(vehicle_string);
+
+                        tripSum += trip_int;
+                        vehicleSum += vehicle_int;
+                    }
+                    String t_v = Integer.toString(tripSum)+","+Integer.toString(vehicleSum);
+                    trips_vehicles.set(t_v);
+                    context.write(key, trips_vehicles);
+                }
+        }
+
+        public static void main(String[] args) throws Exception
+        {
+                Configuration conf = new Configuration();
+                String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+                if (otherArgs.length != 2)
+                {
+                        System.err.println("Usage: Uber <in> <out>");
+                        System.exit(2);
+                }
+                Job job = new Job(conf, "Uber");
+                job.setJarByClass(UBERStudent20200937.class);
+                job.setMapperClass(UberMapper.class);
+                job.setCombinerClass(UberReducer.class);
+                job.setReducerClass(UberReducer.class);
+                job.setOutputKeyClass(Text.class);
+                job.setOutputValueClass(Text.class);
+                FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+                FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+                FileSystem.get(job.getConfiguration()).delete( new Path(otherArgs[1]), true);
+                System.exit(job.waitForCompletion(true) ? 0 : 1);
+        }
 }
