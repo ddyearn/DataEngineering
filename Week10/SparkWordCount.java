42f695ccaaf8bd63abddfee9644f900275747a78
Yoo SeungYeon <115228974+ddyearn@users.noreply.github.com>
2023-06-08 14:15:42 +0900

    Update SparkWordCount.java

--- Week10/SparkWordCount.java
+++ Week10/SparkWordCount.java
@@ -11,40 +11,40 @@ import java.util.regex.Pattern;
 public final class SparkWordCount implements Serializable {
 
 	public static void main(String[] args) throws Exception {
-    
-    if(args.length < 2) {
-      System.err.println("Usage: SparkWordCount <in-file> <out-file>");
-      System.exit(1);
-    }
-		
+
+		if(args.length < 2) {
+			System.err.println("Usage: SparkWordCount <in-file> <out-file>");
+			System.exit(1);
+		}
+
 		SparkSession spark = SparkSession
-      .builder()
-			.appName("SparkWordCount")
-			.getOrCreate();
-		
+				.builder()
+				.appName("SparkWordCount")
+				.getOrCreate();
+
 		JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
-		
-    FlatMapFunction<String, String> fmf = new FlatMapFunction<String, String>() {
-      public Iterator<String> call(String s) {
-        return Arrays.asList(s.split(" ")).iterator();
+
+		FlatMapFunction<String, String> fmf = new FlatMapFunction<String, String>() {
+			public Iterator<String> call(String s) {
+				return Arrays.asList(s.split(" ")).iterator();
 			}
-    };
+		};
 		JavaRDD<String> words = lines.flatMap(fmf);
-		
-    PairFunction<String, String, Integer> pf = new PairFunction<String, String, Integer>() {
+
+		PairFunction<String, String, Integer> pf = new PairFunction<String, String, Integer>() {
 			public Tuple2<String, Integer> call(String s) {
 				return new Tuple2(s, 1);
 			}
 		};
-    JavaPairRDD<String, Integer> ones = words.mapToPair(pf);
-		
-    Function2<Integer, Integer, Integer> f2 = new Function2<Integer, Integer, Integer>() {
+		JavaPairRDD<String, Integer> ones = words.mapToPair(pf);
+
+		Function2<Integer, Integer, Integer> f2 = new Function2<Integer, Integer, Integer>() {
 			public Integer call(Integer x, Integer y) {
 				return x + y;
 			}
 		};
 		JavaPairRDD<String, Integer> counts = ones.reduceByKey(f2);
-		
+
 		counts.saveAsTextFile(args[1]);
 		spark.stop();
 	}
