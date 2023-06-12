ce71a94ebe5aef3d16d8ac21b7c21089aca42c6d
Yoo SeungYeon <115228974+ddyearn@users.noreply.github.com>
2023-06-12 09:29:45 +0900

    Update TopK.java

--- print/Week10/TopK.java
+++ print/Week10/TopK.java
@@ -1,16 +1,3 @@
-import java.util.*;
-import java.io.*;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.mapreduce.lib.input.*;
-import org.apache.hadoop.mapreduce.lib.output.*;
-import org.apache.hadoop.util.GenericOptionsParser;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FSDataOutputStream;
-
 class Emp {
 	public int id;
 	public int salary;
