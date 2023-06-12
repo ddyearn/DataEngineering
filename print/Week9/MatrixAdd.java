536feb028cd3b79f7ed839564e89d5eb10425e75
Yoo SeungYeon <115228974+ddyearn@users.noreply.github.com>
2023-06-12 09:53:39 +0900

    Update MatrixAdd.java

--- print/Week9/MatrixAdd.java
+++ print/Week9/MatrixAdd.java
@@ -1,13 +1,3 @@
-import java.util.*;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.mapreduce.lib.input.*;
-import org.apache.hadoop.mapreduce.lib.output.*;
-import org.apache.hadoop.util.GenericOptionsParser;
-
 public class MatrixAdd {
 	public static class MatrixAddMapper extends Mapper <Object, Text, Text, IntWritable> {
 		private final static IntWritable i_value = new IntWritable(1);
