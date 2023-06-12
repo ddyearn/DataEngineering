ad931fdd3076912a1dce2cc3f9b89da119ac0c31
Yoo SeungYeon <115228974+ddyearn@users.noreply.github.com>
2023-06-12 09:35:50 +0900

    Update ReduceSideJoin.java

--- print/Week11/ReduceSideJoin.java
+++ print/Week11/ReduceSideJoin.java
@@ -1,16 +1,3 @@
-import java.io.IOException;
-import java.util.*;
-
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.mapred.lib.*;
-import org.apache.hadoop.mapreduce.lib.input.*;
-import org.apache.hadoop.mapreduce.lib.output.*;
-import org.apache.hadoop.util.GenericOptionsParser;
-
 public class ReduceSideJoin {
   
 	public static class ReduceSideJoinMapper extends Mapper<Object, Text, Text, Text> {
