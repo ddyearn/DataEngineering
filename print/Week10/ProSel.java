87fd41c04231e504744ff927604c32cfe30eb050
Yoo SeungYeon <115228974+ddyearn@users.noreply.github.com>
2023-06-12 09:26:11 +0900

    Update ProSel.java

--- print/Week10/ProSel.java
+++ print/Week10/ProSel.java
@@ -1,16 +1,3 @@
-import java.util.*;
-import java.io.*;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.mapreduce.lib.input.*;
-import org.apache.hadoop.mapreduce.lib.output.*;
-import org.apache.hadoop.util.GenericOptionsParser;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FSDataOutputStream;
-
 public class ProSel {
 	public static class ProSelMapper extends Mapper<Object, Text, Text, Text>{
 		private Text word = new Text();
