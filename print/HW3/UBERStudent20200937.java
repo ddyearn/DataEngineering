6c52a236a4f3c7ab19eebca6eda19eab86a67200
Yoo SeungYeon <115228974+ddyearn@users.noreply.github.com>
2023-06-12 10:17:13 +0900

    Update UBERStudent20200937.java

--- print/HW3/UBERStudent20200937.java
+++ print/HW3/UBERStudent20200937.java
@@ -1,28 +1,9 @@
-import scala.Tuple2;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.api.java.*;
-import org.apache.spark.api.java.function.*;
-import java.io.Serializable;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-import java.util.regex.Pattern;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-import java.util.*;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.format.TextStyle;
-
 public final class UBERStudent20200937 {
-
     public static void main(String[] args) throws Exception {
-
         if (args.length < 2) {
             System.err.println("Usage: UBER <in-file> <out-file>");
             System.exit(1);
         }
-
         SparkSession spark = SparkSession
             .builder()
             .appName("UBER")
