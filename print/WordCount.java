001734f13811f30cd9fca9eeb80b2b9ff45a3d7c
Yoo SeungYeon <115228974+ddyearn@users.noreply.github.com>
2023-06-12 10:08:02 +0900

    Update WordCount.java

--- print/WordCount.java
+++ print/WordCount.java
@@ -2,7 +2,6 @@ public class WordCount  {
 	public static class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
 		private final static IntWritable one = new IntWritable(1);
 		private Text word = new Text();
-
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException  {
 			StringTokenizer itr = new StringTokenizer(value.toString());
 			while (itr.hasMoreTokens())  {
@@ -14,7 +13,6 @@ public class WordCount  {
 
 	public static class WordCountReducer extends Reducer<Text,IntWritable,Text,IntWritable>  {
 		private IntWritable result = new IntWritable();
-
 		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException  {
 			int sum = 0;
 			for (IntWritable val : values)  {
