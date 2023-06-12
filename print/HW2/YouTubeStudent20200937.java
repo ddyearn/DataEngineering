0ac148a2a2ff3d471ea79de22d0c256c208d7650
Yoo SeungYeon <115228974+ddyearn@users.noreply.github.com>
2023-06-12 10:14:20 +0900

    Update YouTubeStudent20200937.java

--- print/HW2/YouTubeStudent20200937.java
+++ print/HW2/YouTubeStudent20200937.java
@@ -1,16 +1,3 @@
-import java.io.IOException;
-import java.util.*;
-
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.mapreduce.lib.input.*;
-import org.apache.hadoop.mapreduce.lib.output.*;
-import org.apache.hadoop.util.GenericOptionsParser;
-
-
 public class YouTubeStudent20200937 {
 
   public static class Youtube {
@@ -104,26 +91,4 @@ public class YouTubeStudent20200937 {
       }
     }
   }
-
-  public static void main(String[] args) throws Exception {
-    Configuration conf = new Configuration();
-    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-    if (otherArgs.length != 3) 
-    {
-      System.err.println("Usage: Youtube <in> <out>");
-      System.exit(2);
-    }
-    conf.setInt("topK", Integer.parseInt(otherArgs[2]));
-    Job job = new Job(conf, "Youtube");
-    job.setJarByClass(YouTubeStudent20200937.class);
-    job.setMapperClass(YoutubeMapper.class);
-    job.setReducerClass(YoutubeReducer.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(Text.class);
-    FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-    FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
-    FileSystem.get(job.getConfiguration()).delete(new Path(otherArgs[1]), true);
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
-  }
-
 }
