ace9698fa0ccf1ee3ae4915b80b71eee9f670356
Yoo SeungYeon <115228974+ddyearn@users.noreply.github.com>
2023-06-12 09:44:31 +0900

    Update Kmeans.java

--- print/Week13/Kmeans.java
+++ print/Week13/Kmeans.java
@@ -1,20 +1,3 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.io.*;
-import java.util.*;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.mapreduce.lib.input.*;
-import org.apache.hadoop.mapreduce.lib.output.*;
-import org.apache.hadoop.util.GenericOptionsParser;
-
-
 public class Kmeans {
 	public static class KMeansMapper extends Mapper<LongWritable, Text, IntWritable, Text>{
 		private IntWritable one_key = new IntWritable();
