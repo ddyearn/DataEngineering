581fd078d80b94ce41575fde0555c4a154be4ed7
Yoo SeungYeon <115228974+ddyearn@users.noreply.github.com>
2023-06-12 09:47:39 +0900

    Update KmeansCombiner.java

--- print/Week14/KmeansCombiner.java
+++ print/Week14/KmeansCombiner.java
@@ -1,22 +1,6 @@
-import java.util.StringTokenizer;
-import java.io.*;
-import java.util.*;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.mapreduce.lib.input.*;
-import org.apache.hadoop.mapreduce.lib.output.*;
-import org.apache.hadoop.util.GenericOptionsParser;
-
 import week10.Context;
 import week10.Emp;
 import week10.Text;
-
-
 public class KmeansCombiner {
 	public static class KMeansMapper extends Mapper<LongWritable, Text, IntWritable, Text>{
 		private IntWritable one_key = new IntWritable();
