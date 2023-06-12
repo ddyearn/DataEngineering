94c7cab0ffaf2d4af5700c8997c91713a8913b88
Yoo SeungYeon <115228974+ddyearn@users.noreply.github.com>
2023-06-12 09:49:50 +0900

    Update MatrixMulComKey.java

--- print/Week14/MatrixMulComKey.java
+++ print/Week14/MatrixMulComKey.java
@@ -1,17 +1,3 @@
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
-import java.util.*;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.mapred.lib.*;
-import org.apache.hadoop.mapreduce.lib.input.*;
-import org.apache.hadoop.mapreduce.lib.output.*;
-import org.apache.hadoop.util.GenericOptionsParser;
-
 // Composite Key
 class DoubleString implements WritableComparable {
 	String joinKey = new String();
